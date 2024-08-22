0dbc0f20a019863b90343dcc5ddab6b3f33c51c0
DanielG <danikiyesipov@gmail.com>
2024-08-23 01:54:16 +0300

    Updated Codes Backend

---
+++ src/product/dto/product.dto.ts
@@ -0,0 +1,21 @@
+import { IsNumber, IsString } from "class-validator";
+
+export class ProductDto {
+    @IsString()
+    name: string
+
+    @IsString()
+    description: string
+
+    @IsString()
+    image: string
+
+    @IsString()
+    categoryId: string
+
+    @IsNumber()
+    price: number
+
+
+
+}
